9cf2215160de733742294a4fc9a852f10aaf006a
Nicoss01 <n.grillet@devictio.fr>
2012-12-29 22:18:43 +0100

    5

--- erreurs404net.rb
+++ erreurs404net.rb
@@ -71,8 +71,6 @@ cmd = UI::Command.new("City") {
 	dlg.close
 	UI.messagebox "Votre ville mesure #{nbx.to_km} Km x #{nbz.to_km} Km"
 }
-cmd.small_icon = File.dirname(__FILE__)+"\\Erreurs404net\\citys.png"
-cmd.large_icon = File.dirname(__FILE__)+"\\Erreurs404net\\city.png"
 cmd.status_bar_text = "Generate City"
 cmd.tooltip = "Generate a city"
 menu.add_item(cmd)
@@ -92,17 +90,50 @@ cmd2 = UI::Command.new("House") {
 	house2	= ent.add_face [results[3],results[3],0],[results[3],results[1]-results[3],0],[results[0]-results[3],results[1]-results[3],0] ,[results[0]-results[3],results[3],0]
 	house2.reverse!
 	house2.pushpull results[2]-results[3]
-	toit 	= ent.add_face [0,0,results[2]],[0,results[1],results[2]],[0,results[1]/2,results[2]+results[4]],[0,0,results[2]]
-	toit.reverse!
-	toit.pushpull results[0]
-	toit2 	= ent.add_face [results[0],0,results[2]],[results[0],results[1],results[2]],[results[0],results[1]/2,results[2]+results[4]],[results[0],0,results[2]]
-	toit2.reverse!
+	point1 = Geom::Point3d.new (0,results[1]/2,results[2])
+	point2 = Geom::Point3d.new (results[0],results[1]/2,results[2])
+	toit = ents.add_line point1,point2
+	transform = Geom::Transformation.new([0,0,results[4]])
+	ents.transform_entities(transform, toit)
 	house3	= ent.add_face [results[3],results[3],0],[results[3],results[1]-results[3],0],[results[0]-results[3],results[1]-results[3],0] ,[results[0]-results[3],results[3],0]
 	model.commit_operation
 }
-cmd2.small_icon = File.dirname(__FILE__)+"\\Erreurs404net\\citys.png"
-cmd2.large_icon = File.dirname(__FILE__)+"\\Erreurs404net\\city.png"
 cmd2.status_bar_text = "Generate House"
 cmd2.tooltip = "Generate a House"
 menu.add_item(cmd2)
+cmd3 = UI::Command.new("Houses") { 
+	model	= Sketchup.active_model
+	ents		= model.active_entities
+	materials = model.materials
+	prompts 	= ["Longueur", "Largeur","Hauteur","Epaisseur des murs","Hauteur du toit"]
+	values 	= [10.m,10.m,3.m,50.cm,2.m]
+	results 	= UI.inputbox prompts, values, "Parametres de la maison"
+	y=0.m
+	while y<=300.m
+		x=0.m
+		while x<=300.m
+			ratio=rand*1.6+0.9
+			model.start_operation "House in progress"
+			ent 	= Sketchup.active_model.entities
+			house	= ent.add_face [x,y,0],[x,y+results[1],0],[x+results[0],y+results[1],0] ,[x+results[0],y,0]
+			house.reverse!
+			house.pushpull results[2]*ratio
+			house2	= ent.add_face [x+results[3],y+results[3],0],[x+results[3],y+results[1]-results[3],0],[x+results[0]-results[3],y+results[1]-results[3],0] ,[x+results[0]-results[3],y+results[3],0]
+			house2.reverse!
+			house2.pushpull results[2]*ratio-results[3]
+			point1 = Geom::Point3d.new (x,y+results[1]/2,results[2]*ratio)
+			point2 = Geom::Point3d.new (x+results[0],y+results[1]/2,results[2]*ratio)
+			toit = ents.add_line point1,point2
+			transform = Geom::Transformation.new([0,0,results[4]])
+			ents.transform_entities(transform, toit)
+			house3	= ent.add_face [x+results[3],y+results[3],0],[x+results[3],y+results[1]-results[3],0],[x+results[0]-results[3],y+results[1]-results[3],0] ,[x+results[0]-results[3],y+results[3],0]
+			model.commit_operation
+			x	+= results[1]+rand*20.m
+		end
+		y+= results[0]+rand*20.m
+	end
+}
+cmd3.status_bar_text = "Generate Houses"
+cmd3.tooltip = "Generate Houses"
+menu.add_item(cmd3)
 
